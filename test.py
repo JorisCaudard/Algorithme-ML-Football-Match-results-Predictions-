de836c5d3acec47bef748430d2e1b890a4d80f70
jcaudard17@gmail.com <jcaudard17@gmail.com>
2024-03-13 13:25:05 +0100

    COMMIT

--- test.py
+++ test.py
@@ -1,10 +1,32 @@
 from statsbombpy import sb
+import pandas as pd
+import numpy as np
 
 df = sb.events(match_id=3890561)
 
+print(df.shape)
+
 df = df.loc[df['period'] == 1]
 
-EventCountByTeam = df.groupby(by=['team', 'type'])['type'].count().unstack(level=0)
+print(df.shape)
 
-print(EventCountByTeam['Hoffenheim'])
+EventCountByTeam = df.groupby(by=['team', 'type'])['type'].count().unstack(level=1)
 
+EVENT_NAMES = ["Ball Receipt*", "Ball Recovery", "Dispossessed", "Duel", "Camera On", "Block", "Offside", "Clearance", "Interception", "Dribbe", "Shot", "Pressure", "Substitution", "Own Goal Against", "Foul Won", "Foul Committed", "Goal Keeper", "Bad Behaviour", "Player On", "Player Off", "Shield", "Pass", "50/50", "Tactical Shift", "Error", "Miscontrol", 'Dribble', "Dribbled Past", "Injury Stoppage", "Referee Ball-Drop", "Carry"]
+
+TEAM_NAME = "Hoffenheim"
+
+#print(EventCountByTeam.loc['Hoffenheim'].index)
+print(EventCountByTeam.columns)
+
+dfTest = pd.DataFrame(index = [TEAM_NAME])
+
+try :
+    for name in EVENT_NAMES:
+        dfTest.loc[TEAM_NAME, TEAM_NAME + "-" + name] = int(EventCountByTeam.loc[TEAM_NAME, name]) if name in EventCountByTeam.loc[TEAM_NAME].index else np.nan
+    print(dfTest)
+
+except:
+    print("Oopsie !")
+
+print(dfTest['Hoffenheim-Dribbled Past'])
